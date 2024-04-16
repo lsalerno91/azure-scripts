1f04cd3e15724c585684cdc26d858bda49418716
Lorenzo Salerno <lorenzo.salerno@avanade.com>
2024-04-16 12:18:30 +0200

    Added -NotIn Parameter

--- DiagnosticSettings/Remove-AzDiagnosticSettingCustom.ps1
+++ DiagnosticSettings/Remove-AzDiagnosticSettingCustom.ps1
@@ -9,9 +9,13 @@ You can set the scope of the function at subscription level, or at resource grou
 .PARAMETER DestinationLaws (Optional)
 Array of strings with the names of the Log Analytics Workspaces. 
 If you want to remove all the diagnostic settings, leave it empty. 
+If you want to delete all the diagnostic settings that send logs to a specific workspace, specify the workpace name in this parameter. (default)
 If you want to delete all the diagnostic settings, except for the ones sending logs to a specific workspace (e.g. the Sentinel workspace), 
-specify the workspace name in this parameter. 
+specify the workspace name in this parameter and set -NotIn. 
 It can be more than one workspace. 
+
+.Parameter NotIn (Optional)
+If set, delete all the DS that have as a destination a LAW not included in DestinationLaws
 
 .PARAMETER ResourceType (Mandatory)
 Specify the type of Azure resource from which you want to delete the diagnostic settings.
@@ -20,6 +24,7 @@ Allowed values:
 - pip
 - app
 - agw
+- lb
 
 .PARAMETER ResourceGroupName (Optional)
 Set the scope at RG level. 
@@ -30,7 +35,7 @@ If you set this parameter, the DS will not be deleted.
 The function will tell you all the resources that have DS to delete without performing the deletion. 
 
 .EXAMPLE
-Remove-AzDiagnosticSettingCustom -ResourceType nsg -ResourceGroupName conn-tests -DestinationLaws "eeab0244-132c-4ec4-bcc5-57b515a045a2-rg-aksTest-WEU" -DryRun
+Remove-AzDiagnosticSettingCustom -ResourceType nsg -ResourceGroupName conn-tests -DestinationLaws "sentinel-law" -NotIn -DryRun
 
 .NOTES
 VERSION HISTORY
@@ -44,11 +49,15 @@ function Remove-AzDiagnosticSettingCustom {
     param( 
         [string[]]$DestinationLaws,
         [Parameter(Mandatory = $true)]
-        [ValidateSet("nsg", "pip", "app")]
+        [ValidateSet("nsg", "pip", "app", "agw", "lb")]
         [string]$ResourceType,
         [string]$ResourceGroupName,
+        [switch]$NotIn,
         [switch]$DryRun
     )
+
+    # If -NotIn is unset, set $In = $true (default)
+    $In = (-not $NotIn)
 
     $resources = @()
     $resourcesToDelete = @()
@@ -71,6 +80,10 @@ function Remove-AzDiagnosticSettingCustom {
             Write-Host "ResourceType selected: AGW (Application Gateway)."
             $resources = ($PSBoundParameters.ContainsKey('ResourceGroupName')) ? (Get-AzApplicationGateway -ResourceGroupName $ResourceGroupName) : (Get-AzApplicationGateway) 
         }
+        "lb" {
+            Write-Host "ResourceType selected: LB (Load Balancer)."
+            $resources = ($PSBoundParameters.ContainsKey('ResourceGroupName')) ? (Get-AzLoadBalancer -ResourceGroupName $ResourceGroupName) : (Get-AzLoadBalancer) 
+        }
         default {
             Write-Host "ResourceType selected not valid: " $ResourceType
         }
@@ -83,17 +96,28 @@ function Remove-AzDiagnosticSettingCustom {
 
         # check DS that sends logs to Sentinel
         foreach ($ds in $dsList) {
-            if ($ds.WorkspaceId.Split("/")[-1] -notin $DestinationLaws) {
-                $resourcesToDelete += $resource
-                if(!$PSBoundParameters.ContainsKey('DryRun')) {
-                    Write-Host "removing DS " $ds.Name " from resource " $resource.Name
-                    Remove-AzDiagnosticSetting -ResourceId $resource.Id -Name $ds.Name
+            if($in){
+                if ($ds.WorkspaceId.Split("/")[-1] -in $DestinationLaws) {
+                    $resourcesToDelete += $resource
+                    if(!$PSBoundParameters.ContainsKey('DryRun')) {
+                        Write-Host "removing DS " $ds.Name " from resource " $resource.Name
+                        Remove-AzDiagnosticSetting -ResourceId $resource.Id -Name $ds.Name
+                    }
                 }
+            } else {
+                if ($ds.WorkspaceId.Split("/")[-1] -notin $DestinationLaws) {
+                    $resourcesToDelete += $resource
+                    if(!$PSBoundParameters.ContainsKey('DryRun')) {
+                        Write-Host "removing DS " $ds.Name " from resource " $resource.Name
+                        Remove-AzDiagnosticSetting -ResourceId $resource.Id -Name $ds.Name
+                    }
+                }
+
             }
+            
         }   
     }
 
     Write-Host "The following " $ResourceType " have DS to delete: "
     $resourcesToDelete
-
 }
